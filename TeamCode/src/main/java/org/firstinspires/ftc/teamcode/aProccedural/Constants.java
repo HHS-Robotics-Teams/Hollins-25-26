9e5111b0d7b797cc0abfe2ac1973856fa13e48b6
Nightshade17GD <llama0419@icloud.com>
2025-11-05 15:23:54 -0500

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aProccedural/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aProccedural/Constants.java
@@ -2,8 +2,8 @@ package org.firstinspires.ftc.teamcode.aProccedural;
 
 public class Constants {
     /* Constants */
-    public static final double shootingFar = .63;
-    public static final double shootingClose = .39;
+    public static final double shootingFar = .60;
+    public static final double shootingClose = .40;
     public static final double holding = 0;
     public static final double shooting = .5;
 
