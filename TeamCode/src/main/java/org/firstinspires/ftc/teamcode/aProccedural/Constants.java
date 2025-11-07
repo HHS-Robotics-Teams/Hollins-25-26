9d1de398d92ae63f4b5dfc50947e51831f693663
Nightshade17GD <llama0419@icloud.com>
2025-11-07 17:29:36 -0500

    updated short shooting constant

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aProccedural/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aProccedural/Constants.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.aProccedural;
 public class Constants {
     /* Constants */
     public static final double shootingFar = .63;
-    public static final double shootingClose = .41;
+    public static final double shootingClose = .44;
     public static final double holding = 0;
     public static final double shooting = .5;
 
